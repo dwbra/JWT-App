f5408c888c961c5d9639856568478800241b3d9e
Daniel Workman <dworkman@squiz.net>
2022-07-19 18:10:12 +1000

    server setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch(error => console.log(error.message));
