4f007c3a4f9221a89fe375a30d9048f8db3ba788
Daniel Workman <dworkman@squiz.net>
2022-07-19 18:40:09 +1000

    server / mongodb setup

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5001;
 mongoose
   .connect(process.env.MONGODB)
   .then(() =>
