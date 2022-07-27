5ebda7ccedb4e013ba6f054297e34a7b6e073513
Daniel Workman <dworkman@squiz.net>
2022-07-27 22:17:54 +1000

    client auth form for testing

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import AuthForm from "./components/AuthForm/AuthForm";
+
+function App() {
+  return (
+    <>
+      <AuthForm />
+    </>
+  );
+}
+
+export default App;
